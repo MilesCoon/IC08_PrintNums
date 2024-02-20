48a5f0702f4b66f1f48c8a0012c1b3986ddc8e8e
GreyChroma <milesacoon@outlook.com>
2024-02-20 14:00:08 -0800

    Done

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
         printNums(num);
+        System.out.println("\n");
+        printNumsAscending(num);
     }
 
     public static void printNums(int n) throws InterruptedException {
@@ -17,6 +19,20 @@ public class Main {
         }
         else {
             throw new IllegalArgumentException("n must be > 0");
+        }
+    }
+
+    public static void printNumsAscending(int n) {
+        printNumsAscendingRec(1, n);
+    }
+    private static void printNumsAscendingRec(int count, int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("n must be >= 1");
+        } else if (count == n) {
+            System.out.println(n);
+        } else {
+            System.out.print(count + ", ");
+            printNumsAscendingRec(count + 1, n);
         }
     }
 }

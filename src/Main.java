5b1fdb15e191b0973596668f2d6f0a03ec16eed2
GreyChroma <milesacoon@outlook.com>
2024-02-20 14:11:30 -0800

    HarmonicSeries

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,12 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner s = new Scanner(System.in);
         int num = s.nextInt();
+        System.out.println("PrintNums:");
         printNums(num);
-        System.out.println("\n");
+        System.out.println("\nPrintNumsAscending");
         printNumsAscending(num);
+        System.out.println("HarmonicSeries\n" + harmonicSeries(num));
+
     }
 
     public static void printNums(int n) throws InterruptedException {
@@ -34,5 +37,13 @@ public class Main {
             System.out.print(count + ", ");
             printNumsAscendingRec(count + 1, n);
         }
+    }
+
+    public static double harmonicSeries(int n) {
+        if (n > 0) {
+            return (1.0 / n) + harmonicSeries(n - 1);
+        } else if (n == 0) {
+            return 0.0;
+        } else { throw new IllegalArgumentException("n must be >= 0"); }
     }
 }
